92a93767338f28de4febad87a35f8b3e7af6d4c1
albertmvieira <albertmvieira@hotmail.com>
2019-03-12 21:33:04 -0300

    Ajuste Configuração runner e integração Jenkins

---
+++ src/main/java/hooks/MasterHooks.java
@@ -0,0 +1,38 @@
+package hooks;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import utils.DriverFactory;
+
+public class MasterHooks extends DriverFactory {
+	
+	@Before
+	public void setup() {
+		driver = getDriver();
+	}
+	
+	@After
+	public void tearDownAndScreenshotOnFailure(Scenario scenario) throws InterruptedException {
+		try {
+			if (driver != null && scenario.isFailed()) {
+				scenario.embed(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES), "image/png");
+				driver.manage().deleteAllCookies();
+				driver.quit();
+				driver = null;
+			}
+			if(driver != null) {
+				driver.manage().deleteAllCookies();
+				driver.quit();
+				driver = null;
+			}
+		} catch (Exception e) {
+			System.out.println("Methods Failed: tearDownAndScreenshotOnFailure, Exception: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+}
